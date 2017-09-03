a9653dfb66cb37377a0d0319d0bdb40ce9f969d1
Alfredo <alfredo.martinm@gmail.com>
2017-09-03 19:28:42 +0200

    Additional configuration

--- src/test/java/io/alfredux/demo/MapstructLombockSpringbootApplicationTests.java
+++ src/test/java/io/alfredux/demo/MapstructLombockSpringbootApplicationTests.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.alfredux.demo.domain.Source;
 import io.alfredux.demo.domain.Target;
 import io.alfredux.demo.mapper.SourceToTargetMapper;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class MapstructLombockSpringbootApplicationTests {
 
 	@Autowired
@@ -26,6 +26,7 @@ public class MapstructLombockSpringbootApplicationTests {
 	
 	@Test
 	public void mappingWorks() {
+		log.info("MAPPING STUFF");
 		Source source = new Source("demo","demonstration source");
 		Target target = sourceToTargetMapper.toTarget(source);
 		assertEquals(target.getId(), source.getIdentifier());
---
+++ src/test/java/io/alfredux/demo/configuration/TestConfiguration.java
@@ -0,0 +1,10 @@
+package io.alfredux.demo.configuration;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan("io.alfredux")
+public class TestConfiguration {
+
+}
