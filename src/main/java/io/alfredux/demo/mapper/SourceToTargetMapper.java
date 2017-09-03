805eb186a7004843fca881ac2011b4faefff354f
Alfredo <alfredo.martinm@gmail.com>
2017-09-03 22:12:33 +0200

    Added MAPPER variable

--- src/main/java/io/alfredux/demo/mapper/SourceToTargetMapper.java
+++ src/main/java/io/alfredux/demo/mapper/SourceToTargetMapper.java
@@ -3,6 +3,7 @@ package io.alfredux.demo.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
 
 import io.alfredux.demo.domain.Source;
 import io.alfredux.demo.domain.Target;
@@ -10,6 +11,8 @@ import io.alfredux.demo.domain.Target;
 @Mapper(componentModel = "spring")
 public interface SourceToTargetMapper {
 
+	SourceToTargetMapper MAPPER = Mappers.getMapper( SourceToTargetMapper.class );
+	
 	@Mappings({ 
 		@Mapping(source = "characteristics", target = "description"),
 		@Mapping(source = "identifier", target = "id") 
