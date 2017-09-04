85c472390f51fea22dabfb23141d620e29cdd8e8
Alfredo <alfredo.martinm@gmail.com>
2017-09-04 03:09:31 +0200

    Fix
    Add lombok annotation processor tag

--- src/main/java/io/alfredux/demo/mapper/SourceToTargetMapper.java
+++ src/main/java/io/alfredux/demo/mapper/SourceToTargetMapper.java
@@ -3,7 +3,6 @@ package io.alfredux.demo.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.mapstruct.factory.Mappers;
 
 import io.alfredux.demo.domain.Source;
 import io.alfredux.demo.domain.Target;
@@ -11,11 +10,9 @@ import io.alfredux.demo.domain.Target;
 @Mapper(componentModel = "spring")
 public interface SourceToTargetMapper {
 
-	SourceToTargetMapper MAPPER = Mappers.getMapper( SourceToTargetMapper.class );
-	
 	@Mappings({ 
 		@Mapping(source = "characteristics", target = "description"),
 		@Mapping(source = "identifier", target = "id") 
 	})
-	public Target toTarget(Source source);
+	Target toTarget(Source source);
 }
